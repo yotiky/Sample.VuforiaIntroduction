2e90165d30d1cfe9ef78efe80b11d1f5541d57af
yotiky <yotik@gmail.com>
2018-08-21 12:01:45 +0900

    feat: サンプルコード一式

---
+++ Assets/Scripts/MarkerTracker.cs
@@ -0,0 +1,19 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class MarkerTracker : MonoBehaviour, IInputClickHandler
+{
+    public Action<Transform> Click { get; set; }
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if(Click != null)
+        {
+            Click(transform);
+        }
+    }
+}
---
+++ Assets/Scripts/PositionAdjustment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class PositionAdjustment : MonoBehaviour
+{
+    public MarkerTracker marker;
+    public GameObject contents;
+
+    void Start()
+    {
+        marker.Click = m =>
+        {
+            contents.transform.position = m.position;
+            contents.transform.rotation = m.rotation;
+            contents.SetActive(true);
+            VuforiaBehaviour.Instance.enabled = false;
+        };
+
+        contents.SetActive(false);
+    }
+}
